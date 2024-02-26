cb42325e797e3a309fdac10bbecd6bec769869df
Vangelis Sigalas <yourtube12321@gmail.com>
2024-02-26 05:39:45 +0200

    fixed favicon

--- app/layout.js
+++ app/layout.js
@@ -2,25 +2,25 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Raptis Packing",
   description: "Raptis Packing",
-  icons: "/icon.png",
+  icons: {
+    icon: "/favicon.ico", // /public path
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* add this */}
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
-
       <body className={inter.className}>
         <Navbar />
         {children}
