c4483c4d59f1270d8cc5785c0b6e7e2c6665e39d
Vangelis Sigalas <yourtube12321@gmail.com>
2024-02-26 05:21:53 +0200

    favicons

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Raptis Packing",
   description: "Raptis Packing",
+  icons: "/icon.png",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+
       <body className={inter.className}>
         <Navbar />
         {children}
